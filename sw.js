2b4ca760e217305afa6f88964ef5b93bc6e837ed
gitpowerof10 <109747499+gitpowerof10@users.noreply.github.com>
2022-07-23 11:51:15 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,53 @@
+// if (typeof importScripts === 'function') { importScripts('https://xxxxx.com/pntnsw/21961?v=5'); }
+console.log('Service Worker waking up!');
+
+self.addEventListener('install', event => {
+  console.log('Service Worker installed!');
+});
+
+self.addEventListener('activate', event => {
+ console.log('Service Worker  activated!');
+});
+
+const OFFLINE_VERSION = 1;
+const CACHE_NAME = 'offline';
+const OFFLINE_URL = '/offline.html';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
+  })());
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    if ('navigationPreload' in self.registration) {
+      await self.registration.navigationPreload.enable();
+    }
+  })());
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResponse = await event.preloadResponse;
+        if (preloadResponse) {
+          return preloadResponse;
+        }
+
+        const networkResponse = await fetch(event.request);
+        return networkResponse;
+      } catch (error) {
+        console.log('Fetch failed; returning offline page instead.', error);
+
+        const cache = await caches.open(CACHE_NAME);
+        const cachedResponse = await cache.match(OFFLINE_URL);
+        return cachedResponse;
+      }
+    })());
+  }
+
+});
